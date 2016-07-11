5140db9044265fde4d560758d5f3920fa47148eb
KQ <eternal.september@gmail.com>
2016-07-11 13:23:22 -0400

    getting somewhere with options...

--- japanese_kana_practice/js/flashcard.js
+++ japanese_kana_practice/js/flashcard.js
@@ -1,5 +1,10 @@
-var kanaFlash = angular.module('kanaFlash', []);
+var KanaFlash = angular.module('KanaFlash', []);
 
-kanaFlash.controller('KanaController', function KanaController($scope) {
+KanaFlash.controller('KanaController', function($scope) {
 	// stuff here
+	$scope.difficulty_choice = {
+		Basic: false,
+		Voiced: false,
+		Combo: false
+	};
 });
