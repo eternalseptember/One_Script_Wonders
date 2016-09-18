13f91ede69c80815a24b01e65153fa7d8e36cbfb
KQ <eternal.september@gmail.com>
2016-09-18 14:28:36 -0400

    bootstrapping and restarting

--- japanese_kana_practice/js/flashcard.js
+++ japanese_kana_practice/js/flashcard.js
@@ -1,7 +1,49 @@
-var KanaFlash = angular.module('KanaFlash', []);
+var KanaFlash = angular.module('KanaFlash', ['ngRoute']);
 
-KanaFlash.controller('KanaController', function($scope) {
-	$scope.kana_choice = {};
-	$scope.difficulty_choice = {};
 
+KanaFlash.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/main.html',
+			controller: 'KanaController'
+		})
+		.when('/cards', {
+			templateUrl: 'pages/cards.html',
+			controller: 'CardsController'
+		})
+		.when('/chart', {
+			templateUrl: 'pages/chart.html',
+			// Change this later
+			controller: 'KanaController'
+		})
+});
+
+
+KanaFlash.controller('KanaController', ['$scope', '$location', 'choicesService', function($scope, $location, choicesService) {
+
+	$scope.kanaChoice = choicesService.kanaChoice;
+
+	$scope.$watch('kanaChoice', function() {
+		choicesService.kanaChoice = $scope.kanaChoice;
+	});
+
+	$scope.submit = function() {
+		$location.path("/cards");
+	};
+
+}]);
+
+
+KanaFlash.controller('CardsController', ['$scope', 'choicesService', function($scope, choicesService) {
+	$scope.kanaChoice = choicesService.kanaChoice;
+
+	$scope.$watch('kanaChoice', function() {
+		choicesService.kanaChoice = $scope.kanaChoice;
+	});
+
+}]);
+
+
+KanaFlash.service('choicesService', function() {
+	this.kanaChoice = {};
 });
